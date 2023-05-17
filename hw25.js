418a8b46d0b8e67cde3e3d6739f8b04e4ba886af
Povalinna <111307356+Povalinna@users.noreply.github.com>
2023-05-17 15:50:21 +0300

    Update hw25.js
    Add the change

--- hw25.js
+++ hw25.js
@@ -29,28 +29,34 @@ function showProductsHandler(event) {
     }
 }
 function showDetailsHandler(event) {
-    const container = document.querySelector(`.details`);
-    container.innerHTML = ``;
-    const el = event.target;
-    var categoryIndex = el.getAttribute(`data-category`)
-    var productIndex = el.getAttribute(`data-product`);
-
-    const categoryProducts = data[categoryIndex].products;
-    let productsDetails = data[categoryIndex].products[productIndex];
-
-    for (let j = 0; j < data[categoryIndex].length; j++) {
-        console.log(data[categoryIndex]);
-        for (let i = 0; i < data[categoryIndex].products[productIndex]; i++) {
-            const el = document.createElement(`div`);
-
-            el.innerHTML = data[j].products[i];
-            console.log(data[j].products[i]);
-            el.setAttribute(`data-category`, categoryIndex);
-            el.setAttribute(`data-product`, productIndex);
-            el.setAttribute(`data-details`, i, j)
-        }
-        container.appendChild(el);
+     const container = document.querySelector(`.details`); 
+    container.innerHTML = ``; 
+    
+    const categoryIndex = event.target.getAttribute(`data-category`); 
+    const productIndex = event.target.getAttribute(`data-product`); 
+     const productsDetails = data[categoryIndex].products[productIndex]; 
+   
+    const el = document.createElement(`div`);
+     el.setAttribute(`id`,`prodDet`);
+     el.innerHTML = productsDetails.name;
+        const elem = document.createElement(`div`);
+        elem.setAttribute(`id`,`prodPrice`);
+        elem.innerHTML = productsDetails.price;
+           container.appendChild(el); 
+           container.appendChild(elem);
+     const btn = document.createElement(`button`); 
+        btn.innerHTML = "купити";
+        btn.setAttribute(`id`,`btn`)
+         container.appendChild(btn);
+             btn.onclick = function(){
+             this.style.color = `red`;
+             this.innerHTML ="товар куплений";
+                 setTimeout(() => {
+                 document.getElementById('prodDet').style.display = `none`;
+                 document.getElementById('prodPrice').style.display = `none`;
+                 document.getElementById(`btn`).style.display=`none`;
+                 showProductsHandler();
+                 document.getElementById(`catProd`).style.display=`none`},2000);}
     }
-}
 
 showCategories();
